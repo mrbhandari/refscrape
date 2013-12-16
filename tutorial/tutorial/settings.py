cd602dfb3387f1b2eac67db625561685bee19a5a
Rahul Bhandari <rahul@bloomreach.com>
2013-12-15 21:50:42 -0800

    uipdate

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -10,6 +10,17 @@ BOT_NAME = 'tutorial'
 
 SPIDER_MODULES = ['tutorial.spiders']
 NEWSPIDER_MODULE = 'tutorial.spiders'
+EXTENSIONS = {'scrapy.contrib.logstats.LogStats': 1}
+LOGSTATS_INTERVAL = 60.0
+RETRY_TIMES = 4
+CONCURRENT_REQUESTS = 320
+CONCURRENT_REQUESTS_PER_DOMAIN = 120
+CONCURRENT_ITEMS = 2000
+DOWNLOAD_DELAY = 0.75
+#DOWNLOADER_MIDDLEWARES = {
+#        'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware' : None,
+#        'Tutorial.rotate_useragent.RotateUserAgentMiddleware' :400
+#    }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
